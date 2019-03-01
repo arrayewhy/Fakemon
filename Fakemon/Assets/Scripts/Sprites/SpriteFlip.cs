c17b72f335fe5ea2d3ac6f8a1e8e8252b420d9f7
God <netherchef@gmail.com>
2019-03-01 17:24:53 +0800

    Started [MonMove]

---
+++ Fakemon/Assets/Scripts/Sprites/SpriteFlip.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlip : MonoBehaviour {
+
+	public void FlipSprite (Transform trans) {
+		Vector2 tempScale = trans.localScale;
+		tempScale.x *= -1;
+		trans.localScale = tempScale;
+	}
+}
