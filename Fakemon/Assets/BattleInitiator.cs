007bb11e09d9a9d051949f4a92991c52fca46ddb
Nomad <netherchef@gmail.com>
2019-03-09 15:50:42 +0800

    Started [Sprite Direction]

---
+++ Fakemon/Assets/BattleInitiator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleInitiator : MonoBehaviour
+{
+	// Scripts
+
+	FindObjectByTag findObjectByTag;
+
+	public Emotes emotes;
+
+	// Emote Variables
+
+	int flashDuration = 1;
+
+	// Enumerators
+
+	IEnumerator standbyBattle;
+
+	IEnumerator flashAlert;
+
+	private void Start ()
+	{
+		// Scripts
+
+		findObjectByTag = GetComponent<FindObjectByTag> ();
+
+		#region Start Operations ...............................................
+
+		standbyBattle = StandbyBattle ();
+		StartCoroutine (standbyBattle);
+
+		#endregion
+	}
+
+	#region Standby ____________________________________________________________
+
+	IEnumerator StandbyBattle ()
+	{
+		while (!findObjectByTag.seeTarget) yield return null;
+
+		InitiateBattle ();
+	}
+
+	#endregion
+
+	#region Battle _____________________________________________________________
+
+	void InitiateBattle ()
+	{
+		FlashAlert ();
+	}
+
+	#endregion
+
+	#region Emote ______________________________________________________________
+
+	void FlashAlert ()
+	{
+		flashAlert = emotes.FlashAlert (flashDuration);
+		StartCoroutine (flashAlert);
+	}
+
+	#endregion
+}
---
+++ Fakemon/Assets/SpriteDirection.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteDirection : MonoBehaviour
+{
+	// Components
+
+	SpriteRenderer spriteRenderer;
+
+	// Scripts
+
+	Direction direction;
+
+	// Sprites
+
+	[Header ("Sprites")]
+	public Sprite up;
+	public Sprite down;
+	public Sprite side;
+
+	// Variables
+
+	public bool shouldLookSideWays;
+	public bool shouldLookUp;
+
+	private void Start ()
+	{
+		// Components
+
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+
+		// Scripts
+
+		direction = GetComponent<Direction> ();
+	}
+
+	private void Update ()
+	{
+		if (shouldLookSideWays)
+		{
+			if (ShouldFaceRight ()) LookRight ();
+			else LookLeft ();
+		}
+		else
+		{
+			if (shouldLookUp) LookUp ();
+			else LookDown ();
+		}
+	}
+
+	#region Look _______________________________________________________________
+
+	void LookLeft ()
+	{
+		if (spriteRenderer.sprite != side) spriteRenderer.sprite = side;
+	}
+
+	void LookRight ()
+	{
+		if (spriteRenderer.sprite != side) spriteRenderer.sprite = side;
+	}
+
+	void LookUp ()
+	{
+		if (spriteRenderer.sprite != up) spriteRenderer.sprite = up;
+	}
+
+	void LookDown ()
+	{
+		if (spriteRenderer.sprite != down) spriteRenderer.sprite = down;
+	}
+
+	#endregion
+
+	#region Direction __________________________________________________________
+
+	bool ShouldFaceRight ()
+	{
+		return direction.facingRight;
+	}
+
+	#endregion
+}
